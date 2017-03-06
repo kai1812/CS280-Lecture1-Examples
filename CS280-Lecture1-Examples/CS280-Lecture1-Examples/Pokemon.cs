353255b9d96415b13edb05a8e88077e2e4111b9c
kai1812 <kai1812@gmail.com>
2017-03-07 00:36:27 +0800

    新增pokemon類別

---
+++ CS280-Lecture1-Examples/CS280-Lecture1-Examples/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Lecture1_Examples
+{
+    class Pokemon
+    {
+    }
+}
